a555c943ba9d288db30ac8a544e7f4c940b1a516
Ilya <ilaybelous37@gmail.com>
2024-01-05 18:41:22 +0200

    create two new block

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
-console.log('hello')
+let list = document.querySelectorAll('li');
+let link = document.querySelectorAll('#link')
+
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        let listItem = link.closest('li');
+        listItem.classList.toggle('active');
+    });
+});
